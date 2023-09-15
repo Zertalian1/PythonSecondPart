8a3e3cf72ca8af97528c4c8831c1772dc7cbf459
zertalian <Zertalian@mail.ru>
2023-09-15 11:01:29 +0700

    add db

--- myCrudApp/entity/University.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class University(models.Model):
-    full_name = models.CharField(max_length=50)
-    short_name = models.CharField(max_length=10)
-    date_of_creation = models.DateField()
--- myCrudApp/models.py
+++ myCrudApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class University(models.Model):
+    full_name = models.CharField(max_length=50)
+    short_name = models.CharField(max_length=10)
+    date_of_creation = models.DateField()
+
+
+class Student(models.Model):
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    FCs = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    year_of_admission = models.DateField()
