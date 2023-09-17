d39b15da9eb269b841e1bfdeb6999be53f49b0b4
Zertalian1 <d.kurdyukov@g.nsu.ru>
2023-09-17 16:00:14 +0700

    конец

--- myCrudApp/admin.py
+++ myCrudApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from myCrudApp.models import University, Student
+
+class UniversityAdmin(admin.ModelAdmin):
+    #Данная переменная указывает на поля, которые будут выводится в списке продуктов
+    list_display = ('id', 'full name', 'short name', 'date of creation')
+
+admin.site.register(University)
+admin.site.register(Student)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'FCs', 'date of birth', 'year of admission', 'university id')
--- myCrudApp/models.py
+++ myCrudApp/models.py
@@ -6,9 +6,15 @@ class University(models.Model):
     short_name = models.CharField(max_length=10)
     date_of_creation = models.DateField()
 
+    def __str__(self):
+        return f"{self.full_name}_{self.short_name}_{self.date_of_creation}"
+
 
 class Student(models.Model):
     university = models.ForeignKey(University, on_delete=models.CASCADE)
     FCs = models.CharField(max_length=50)
     date_of_birth = models.DateField()
     year_of_admission = models.DateField()
+
+    def __str__(self):
+        return f"{self.FCs}_{self.date_of_birth}_{self.year_of_admission}_{self.university.id}"
